8a33e386c53880c4872837dfe389fa8f45a7a163
Leila Farajnezhad <farajnezhad.leila@gmail.com>
2025-08-31 17:19:37 +0330

    object supper farajnezhad up

--- object_super.py
+++ object_super.py
@@ -1,53 +1,93 @@
-class Prudact:
-    def __init__(self, name , group, brand , price):
+class Product:
+    def __init__(self, name , brand , price):
         self.name = name
-        self.group = group
+        self.group = "product"
         self.brand = brand
         self.price = price
 
-    def save(self):
-        print(f"{self.name} has been saved")
+    # def save(self):
+    #     print(f"{self.name} has been saved")
+    def __repr__(self):
+        return f"{self.__dict__}"
 
-class Electronic:
-    def __init__(self, voltag ,name, group, brand , price):
-        self.voltag = voltag
-        self.name = name
-        self.group = group
-        self.brand = brand
-        self.price = price
-
-
-class Clothes(Prudact):
-    def size(self):
-        print(f"{self.name} size")
-
-class Foods(Prudact):
-    def ex_data(self):
-        print(f"{self.name} ex_data")
-
-    def pn_data(self):
-        print(f"{self.name} pn_data")
+class Electronic(Product):
+    def __init__(self, voltage ,name,  brand , price):
+        super().__init__(name, brand, price)
+        self.voltage = voltage
+        self.group += "electronic"
 
 
-class Health(Prudact):
-    def ex_data(self):
-        print(f"{self.name} ex_data")
 
-class Laptops(Prudact,Electronic):
-    pass
-
-class Mobile(Prudact, Electronic):
-    pass
-
-class Icecream(Foods,Prudact):
-    pass
+class Clothes(Product):
+    def __init__(self, name,  brand , price,size):
+        super().__init__(name, brand, price)
+        self.size = size
+        self.group += "clothes"
 
 
-class Drink(Foods,Prudact):
-    pass
 
-class Shampoo(Health,Prudact):
-    pass
+class Food(Product):
+    def __init__(self, name,  brand , price,ex_data,p_data):
+        super().__init__(name, brand, price)
+        self.ex_data = ex_data
+        self.p_data = p_data
+        self.group += "food"
 
-class Cream(Health,Prudact):
-    pass
+
+class Health(Product):
+    def __init__(self, name,  brand , price,ex_data,p_data):
+        super().__init__(name, brand, price)
+        self.ex_data = ex_data
+        self.p_data = p_data
+        self.group += "health"
+
+
+class Laptop(Electronic):
+    def __init__(self, name,  brand , price,ram,cpu):
+        super().__init__(name, brand, price)
+        self.ram = ram
+        self.cpu = cpu
+        self.group+= "laptop"
+
+
+class Mobile( Electronic):
+    def __init__(self, name,  brand , price,inch):
+        super().__init__(name, brand, price)
+        self.inch = inch
+        self.group+= "mobile"
+
+
+class Icecream(Food):
+    def __init__(self, name,  brand , price,ex_data,p_data):
+        super().__init__(name, brand, price)
+        self.ex_data = ex_data
+        self.p_data = p_data
+        self.group += "icecream"
+
+
+
+
+class Drink(Food):
+    def __init__(self, name,  brand , price,ex_data,p_data):
+        super().__init__(name, brand, price)
+        self.ex_data = ex_data
+        self.p_data = p_data
+        self.group += "drink"
+
+
+class Shampoo(Health,):
+    def __init__(self, name,  brand, price, ex_data, p_data):
+        super().__init__(name, brand, price)
+        self.ex_data = ex_data
+        self.p_data = p_data
+        self.group += "shampoo"
+
+
+class Cream(Health,):
+    def __init__(self, name,  brand, price, ex_data, p_data):
+        super().__init__(name, brand, price)
+        self.ex_data = ex_data
+        self.p_data = p_data
+        self.group += "cream"
+
+
